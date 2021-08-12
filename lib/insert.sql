a082a4247597a9c1cd66e92a74da02aa5476c05a
Elisa Shapiro <elisashapiro@gmail.com>
2021-08-12 16:56:44 -0400

    commit tree bcfada71eb8fe3fc251062ba4aebf72ee7d88c88

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 13, "M", "brown", "happy", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 11, "M", "black", "angry", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 12, "F", "white", "nervous", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 17, "M", "brown", "angry", 0);
+INSERT INTO bears VALUES (5, "Melissa", 7, "F", "brown", "happy", 1);
+INSERT INTO bears VALUES (6, "Grinch", 9, "M", "brown", "angry", 1);
+INSERT INTO bears VALUES (7, "Wendy", 15, "F", "brown", "happy", 1);
+INSERT INTO bears VALUES (8, null, 15, "M", "dark brown", "angry", 0);
